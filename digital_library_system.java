b0f72daa3c0b696b2b34d9ab586495c1f7b7c794
anjupravasahoo88-code <anjupravasahoo88@gmail.com>
2026-06-08 16:16:46 +0530

    Update digital_library_system.java

--- digital_library_system.java
+++ digital_library_system.java
@@ -1,43 +1,109 @@
-/*
-Digital Library System
+import java.io.*;
 
-Create:
 
-Book
-│
-| -  EBook
-│
-| - PrintedBook
+class Book {
+ int id;
+ String title;
+ double price;
 
-Requirements:
+ Book(int id, String title, String priceStr) {
+     this.id = id;
+     this.title = title;
 
-Override:
+    
+     this.price = Double.parseDouble(priceStr);
+ }
 
-showBookType()
+ void showBookType() {
+     System.out.println("General Book");
+ }
 
-for each child class.
 
-Overloading:
+ void searchBook(int id) {
+     if (this.id == id) {
+         System.out.println("Book Found: " + title);
+     } else {
+         System.out.println("Book Not Found");
+     }
+ }
 
-Create:
+ void searchBook(String title) {
+     if (this.title.equalsIgnoreCase(title)) {
+         System.out.println("Book Found: " + this.title);
+     } else {
+         System.out.println("Book Not Found");
+     }
+ }
+}
 
-searchBook(int id)
 
-searchBook(String title)
+class EBook extends Book {
 
-Wrapper Class Requirement:
+ EBook(int id, String title, String priceStr) {
+     super(id, title, priceStr);
+ }
 
-Convert:
+ @Override
+ void showBookType() {
+     System.out.println("Book Type: E-Book");
+ }
+}
 
-Book Price (String)
 
-into numeric value using Wrapper Classes.
+class PrintedBook extends Book {
 
-File Handling:
+ PrintedBook(int id, String title, String priceStr) {
+     super(id, title, priceStr);
+ }
 
-Store all book details in:
+ @Override
+ void showBookType() {
+     System.out.println("Book Type: Printed Book");
+ }
+}
+public class DigitalLibrarySystem {
+	public static void main(String[] args) {
 
-library.txt
+        EBook b1 = new EBook(101, "Java Programming", "499.50");
+        PrintedBook b2 = new PrintedBook(102, "Data Structures", "650.75");
 
-and read them back.
-*/
+       
+        b1.showBookType();
+        b2.showBookType();
+
+      
+        b1.searchBook(101);
+        b2.searchBook("Data Structures");
+
+      
+        try {
+            FileWriter fw = new FileWriter("library.txt");
+
+            fw.write("ID: " + b1.id + ", Title: " + b1.title +
+                    ", Price: " + b1.price + "\n");
+
+            fw.write("ID: " + b2.id + ", Title: " + b2.title +
+                    ", Price: " + b2.price + "\n");
+
+            fw.close();
+
+            System.out.println("\nBook details stored in library.txt");
+
+         
+            BufferedReader br = new BufferedReader(
+                    new FileReader("library.txt"));
+
+            String line;
+
+            System.out.println("\nReading File Contents:");
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("File Error: " + e.getMessage());
+        }
+    }
+}
